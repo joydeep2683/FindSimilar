f5aeb507e2fc4719e2006e8f5c0164d11def7948
Joydeep Ballabh <joydeep@fabulyst.com>
2020-06-04 11:33:20 +0530

    new base dev branch created

--- object_detector/util.py
+++ object_detector/util.py
@@ -1,5 +1,15 @@
-def funcname(parameter_list):
-    print(2)
-    return 2
+def get_obj_img_by_bbox(frame, bbox):
+    """[Given a image and a bounding box it will return cropped image]
 
-funcname("")
+    Args:
+        frame ([3d numpy array]): [It's an image]
+        bbox ([list]): [bounding box with x, y, width, height]
+
+    Returns:
+        [np.array]: [cropped image]
+    """
+    x, y = bbox[0], bbox[1]
+    w, h = bbox[2], bbox[3]
+    obj_img = frame[y:y+h, x:x+w, :]
+    rgb_frame = obj_img[:, :, ::-1]
+    return rgb_frame
